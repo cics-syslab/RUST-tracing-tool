199981aa6a56a5f3e226e7bd67e03f767d40b0d4
Kush Srivastava <kushagra2410@gmail.com>
2023-04-24 05:04:12 +0000

    Do sample run for CPP, copy in function that was used for CPP in a prior test

--- asmblr.py
+++ asmblr.py
@@ -31,6 +31,9 @@ def startCorCPP(inputFile):
   command = "g++ -g -S " + inputFile + " -o " + splitFileName(inputFile)[0]
   result = subprocess.run(command, stdout=subprocess.PIPE, shell=True)
   didItRun(result)
+  command = "mv " + splitFileName(inputFile)[0] + " " + splitFileName(inputFile)[0] + ".s"
+  result = subprocess.run(command, stdout=subprocess.PIPE, shell=True)
+  didItRun(result)
 
 def figureLanguage(inputFile):
     if inputFile.endswith('.rs'):
@@ -56,7 +59,7 @@ def main_assembly(inputFile):
         # Remove any comments
         line = re.sub(r'#.*$', '', line)
         # Extract the instruction mnemonic
-        match = re.match(r'^\s*[0-9a-f]+:\s+([a-zA-Z]+(\s+[a-zA-Z]+)?)', line, re.IGNORECASE)
+        match = re.match(r'^\s*([a-zA-Z0-9_]+)\s', line)
         if match:
             instructions.append(match.group(1))
     # Initialize the register and memory state
@@ -65,8 +68,100 @@ def main_assembly(inputFile):
     # Start executing instructions
     pc = 0
     print(instructions)
-    #trace(instructions, registers, memory)
-  
+    trace_for_cpp(instructions, registers, memory)
+
+def trace_for_cpp(instructions, registers, memory):
+    pc = 0
+    stack = []
+    while pc < len(instructions):
+        # Get the current instruction
+        instr = instructions[pc]
+        # Print the current instruction and state
+        print(f"Instruction: {instr}")
+        print(f"Registers: {registers}")
+        print(f"Memory: {memory}")
+        print(f"Stack: {stack}")
+        # Update the program counter
+        pc += 1
+        # Decode and execute the instruction
+        if instr.startswith("mov"):
+            # Example: mov eax, 0x12345678
+            match = re.match(r'^\s*mov\s+([a-z]+),\s+(.*)$', instr, re.IGNORECASE)
+            reg = match.group(1)
+            value = match.group(2)
+            if value.startswith("0x"):
+                # Hexadecimal value
+                registers[reg] = int(value, 16)
+            else:
+                # Decimal value
+                registers[reg] = int(value)
+        elif instr.startswith("add"):
+            # Example: add eax, ebx
+            match = re.match(r'^\s*add\s+([a-z]+),\s+([a-z]+)$', instr, re.IGNORECASE)
+            reg1 = match.group(1)
+            reg2 = match.group(2)
+            registers[reg1] += registers[reg2]
+        elif instr.startswith("sub"):
+            # Example: sub eax, 0x10
+            match = re.match(r'^\s*sub\s+([a-z]+),\s+(.*)$', instr, re.IGNORECASE)
+            reg = match.group(1)
+            value = match.group(2)
+            if value.startswith("0x"):
+                # Hexadecimal value
+                registers[reg] -= int(value, 16)
+            else:
+                # Decimal value
+                registers[reg] -= int(value)
+        elif instr.startswith("jmp"):
+            # Example: jmp 0x12345678
+            match = re.match(r'^\s*jmp\s+(.*)$', instr, re.IGNORECASE)
+            target = int(match.group(1), 16)
+            pc = target
+        elif instr.startswith("je"):
+            # Example: je 0x12345678
+            match = re.match(r'^\s*je\s+(.*)$', instr, re.IGNORECASE)
+            target = int(match.group(1), 16)
+            if registers['zf'] == 1:
+                pc = target
+        elif instr.startswith("call"):
+            # Example: call func
+            match = re.match(r'^\s*call\s+(.*)$', instr, re.IGNORECASE)
+            target = match.group(1)
+            stack.append(pc)
+            pc = target
+        elif instr.startswith("ret"):
+            # Example: ret
+            pc = stack.pop()
+        elif instr.startswith("push"):
+            # Example: push eax
+            match = re.match(r'^\s*push\s+([a-z]+)$', instr, re.IGNORECASE)
+            reg = match.group(1)
+            stack.append(registers[reg])
+        elif instr.startswith("pop"):
+            # Example: pop eax
+            match = re.match(r'^\s*pop\s+([a-z]+)$', instr, re.IGNORECASE)
+            reg = match.group(1)
+            registers[reg] = stack.pop()
+        elif instr.startswith("cmp"):
+            # Example: cmp eax, 0x10
+            match = re.match(r'^\s*cmp\s+([a-z]+),\s+(.*)$', line, re.IGNORECASE)
+            reg = match.group(1)
+            value = match.group(2)
+            if value.startswith("0x"):
+                # Hexadecimal value
+                value = int(value, 16)
+            else:
+                # Decimal value
+                value = int(value)
+            if registers[reg] == value:
+                registers['zf'] = 1
+            elif registers[reg] > value:
+                registers['cf'] = 0
+                registers['zf'] = 0
+            else:
+                registers['cf'] = 1
+                registers['zf'] = 0
+
 def trace(instructions, registers, memory):
     pc = 0
     stack = []
