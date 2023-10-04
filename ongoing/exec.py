1ea5489e7405d80d40940e6c520a75440aa49e78
suobset <kushagra2410@gmail.com>
2023-10-04 10:58:00 -0400

    Add boilerplate code that goes across languages

--- ongoing/exec.py
+++ ongoing/exec.py
@@ -1,12 +1,19 @@
 # Essentially a makefile but in Python
 
-import subprocess
+import subprocess   # Executes terminal commands
 
+"""
+Steps outlined for a C file
 
+1: Execute the pipeline, and get it to the Assembly Stage.  ```def execute_C()```
+2: Halt this at the Assembly, and make changes.             ```def edit_C()```
+3: Compile this assembly file to an Executable.             ```def compile_C()```
+"""
 def execute_C(filename):
     # Define the command you want to execute as a list of strings
     rust_file = filename + ".cpp"
-    command = ["clang++ -emit-llvm -S *.cpp", "llvm-as *.ll", "llc *.bc --o main.s", "clang++ main.s -o main.native"]
+    # Assume name of file is main (for now)
+    command = ["clang++ -emit-llvm -S main.cpp", "llvm-as main.ll", "llc main.bc --o main.s"]
     
     try:
         # Run the command and capture the output
@@ -26,6 +33,34 @@ def execute_C(filename):
         print("Command not found. Make sure it's installed and in your PATH.")
     except Exception as e:
         print(f"An error occurred: {str(e)}")
+    # Call edit_C() and continue to step 2
+    return edit_C()
+
+def edit_C():
+    inputFile = "./main.s"
+    with open(inputFile, 'r') as f:
+        assembly_code = f.read()
+    print(f)
+def compile_C():
+    try:
+        # Compile the edited assembly file into a native executable
+        result = subprocess.run("clang++ main.s -o main.native", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
+
+        # Print the output
+        print("Compile output: ")
+        print(result.stdout)
+
+        #Check for errors, and print any
+        if result.stderr:
+            print("Compile Error: ")
+            print(result.stderr)
+    except subprocess.CalledProcessError as e:
+        print(f"Command failed with return code {e.returncode}: {e.cmd}")
+    except FileNotFoundError:
+        print("Command not found. Make sure it's installed and in your PATH.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")  
+    return 0
 
 def execute_R(filename):
     # Define the command you want to execute as a list of strings
