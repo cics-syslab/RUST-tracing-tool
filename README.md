a98d24030fb6d603f5bd05a609214722932a9bf0
suobset <kushagra2410@gmail.com>
2024-05-19 12:02:10 -0400

    Add new build with Estimated Time of Completion

--- README.md
+++
@@ -1,7 +0,0 @@
-# RUST-tracing-tool
-
-A tracing tool to get metrics for performance analysis between C++ and RUST. Proper Website Under Development, with an ETA of 04/17/2024.
-
-![Screenshot of Tracer](./1.png)
-
-Created by Kush S.: https://skushagra.com
--- iCons4-backend/docusaurus.config.js
+++ iCons4-backend/docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'LLVM Compilation Performance Metrics',
-  tagline: 'Kush Srivastava | Fall 2023 iCons 4 | CHC Honors Thesis',
+  title: 'tra86: The Asembly Tracer Toolchain',
+  tagline: 'Website Under Construction | ETA 05/25/2023',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -54,7 +54,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'LLVM Metrics',
+        title: 'RUST & C++ Metrics',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
